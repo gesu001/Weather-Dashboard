26546a57bcc74ede620f98a6819279d4c7517f6e
Ge Su <gegege.su@gmail.com>
2023-05-04 00:50:02 +1000

    added 5 day forcast

--- assets/script.js
+++ assets/script.js
@@ -4,6 +4,7 @@ var APIKey = "74c48057670478a17f27408eb3690c3b"
 var cityListEl = document.getElementById("cityList")
 var cityNames = [];
 var displayAreaEl = document.getElementById("displayArea")
+
 //var today = dayjs().format('YYYY-MM-DD');
 //var nextday = dayjs().add(1, "day").format('YYYY-MM-DD');
 //console.log(today)
@@ -20,13 +21,22 @@ function getApi() {
     .then(function (data) {
         console.log(data)
       var iconCode = data.weather[0].icon
-      //console.log(iconUrl= 'http://openweathermap.org/img/w/'+ iconCode +'.png');
-
       var city = document.getElementById('cityname')
       var currentWeather = document.getElementById('currentWeather')
       var date = dayjs.unix(data.dt).format('YYYY-MM-DD');
-      city.textContent = data.name + " (" + date + ") " + iconCode;
+      console.log(date)
+      var weatherIcon = document.createElement("img");
+      var iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+      var cityName = data.name
+      city.textContent = cityName + " (" + date + ") "
+      console.log(iconURL)
+      weatherIcon.setAttribute("src",iconURL)
+      city.appendChild(weatherIcon)
       currentWeather.innerText = 'Temp: '+ data.main.temp + '\xB0F'+ '\n' + 'Wind: '+ data.wind.speed + ' MPH' + '\n' + 'Humidity: '+ data.main.humidity + '%'
+
+      cityNames.push(cityName);
+      localStorage.setItem("cityNames", JSON.stringify(cityNames))
+    
     });
   }
 
@@ -61,6 +71,7 @@ function getApi() {
         var dailyHumidity = dailyList.main.humidity;
         var dailyWind = dailyList.wind.speed
         var dailyIcon = dailyList.weather[0].icon
+        var dailyIconURL = "https://openweathermap.org/img/wn/" + dailyIcon + "@2x.png";
 
 
         var divEl = document.createElement('div')
@@ -69,12 +80,13 @@ function getApi() {
         var iconEl = document.createElement('li')
         var tempEl = document.createElement('li')
         var humidityEl = document.createElement('li')
-        var windEl = document.createElement('li')        
+        var windEl = document.createElement('li')
+        var iconImgEl = document.createElement('img')       
 
-        ulEl.setAttribute("data-index", "i");
+        ulEl.setAttribute("data-index", i);
         divEl.setAttribute("class", "col-12 col-sm-6 col-lg-2 mb-3");
-
         ulEl.setAttribute("class","card card-1 bg-dark text-light");
+        iconImgEl.setAttribute("src", dailyIconURL)
 
         displayAreaEl.appendChild(divEl)
         divEl.appendChild(ulEl)
@@ -83,14 +95,14 @@ function getApi() {
         ulEl.appendChild(tempEl);
         ulEl.appendChild(humidityEl);
         ulEl.appendChild(windEl);
-        
+        iconEl.appendChild(iconImgEl);
 
         dateEl.textContent = dailyDate;
-        iconEl.textContent = dailyIcon;
+        //iconEl.textContent = dailyIcon;
         tempEl.textContent = "Temp: " + dailyTemp;
         humidityEl.textContent = "Humidity: " + dailyHumidity;
         windEl.textContent = "Wind: " + dailyWind;
-        //console.log(iconUrl= 'http://openweathermap.org/img/w/'+ iconCode +'.png');   
+        //console.log(iconUrl= 'http://openweathermap.org/img/wn/'+ iconCode +'.png');   
       }
     
     });
@@ -110,21 +122,21 @@ function getApi() {
   }
 
   function init () {
-    var storedCitys = JSON.parse(localStorage.getItem("cityNames"));
-    if (storedCitys !== null) {
-      cityNames = storedCitys;
+    var storedCity = JSON.parse(localStorage.getItem("cityNames"));
+    if (storedCity !== null) {
+      cityNames = storedCity;
     }
     renderCityNames();
   }
 
-  function saveCityNames(cityName) {
-    if (cityName === "") {
-      return;
-    }
-    cityNames.push(cityName);
-    inputEl.value = ""; 
-      localStorage.setItem("cityNames", JSON.stringify(cityNames))
-  }
+  //function saveCityNames(cityName) {
+  //  if (cityName === "") {
+   //   return;
+   // }
+  //  cityNames.push(cityName);
+   // inputEl.value = ""; 
+   //   localStorage.setItem("cityNames", JSON.stringify(cityNames))
+ // }
 
 // Add click event to cityListEl
 cityListEl.addEventListener("click", function(event) {
@@ -143,9 +155,12 @@ cityListEl.addEventListener("click", function(event) {
 btn.addEventListener('click', function(event){
   event.preventDefault();
   var cityName = inputEl.value.trim();
+  if (!cityName){
+    return}
+
   getApi();
   getDailyApi();
-  saveCityNames(cityName);
+  //saveCityNames(cityName);
   renderCityNames();
 });
 
